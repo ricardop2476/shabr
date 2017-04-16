2b512f9ff0e323c927556f3cd94b4d9b43c257cf
ricardop2476 <ricardop2476@hstat.org>
2017-04-16 04:15:12 +0000

    add user comment

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
+    
+    $('button').click(function() {
+        var commentName = $('#userName').val();
+        $('#name').html(commentName);
+              
+        var commentText = $('#userComment').val();
+        $('#comment').html('"'+commentText+'"');
+    });
 });
